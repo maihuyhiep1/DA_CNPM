77a30fb332cd63ae68a0f41bbb612f923d8d1992
KhoaPham359 <145217965+KhoaPham359@users.noreply.github.com>
2024-10-27 17:15:29 +0700

    test for GetUserByID

--- src/services/test-UserServices.js
+++ src/services/test-UserServices.js
@@ -1,20 +1,41 @@
 //TEST FILE
 
-const createUser = require('./user-services');
+const userService = require('./user-services');
 
-
-(async () => {
+// Test function for creating a user
+async function testCreateUser() {
   try {
     const testBody = {
       name: 'Test User',
       email: 'testuser@example.com',
       password: 'TestPassword123',
     };
-
-    const result = await createUser.createUser(testBody);
-    console.log(result);
+    const result = await userService.createUser(testBody);
+    console.log("testCreateUser:", result);
   } catch (error) {
-    console.error('Error creating user:', error);
+    console.error("Error in testCreateUser:", error);
   }
+}
+
+testGetUserByID = async (id) => {
+  try {
+    let user = await userService.getUserByID(id);
+    console.log(user);
+  } catch (e) {
+    if (e.name === 'SequelizeEmptyResultError') {
+      console.log('User not found. do something!');
+    }
+    else {  console.log("Error in testGetUserByID:", e); }
+  }
+}
+
+
+// Run all tests in sequence
+(async () => {
+  // await testCreateUser();
+  await testGetUserByID(0);
+  // Add more test functions as needed and call them here
 })();
+
+
 // Use node {directory}/test-UserServices.js to run
