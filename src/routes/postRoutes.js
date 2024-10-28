094f2e892387e3fad44bcfded425d9b40dccaedf
KeHamTruyen <146626863+KeHamTruyen@users.noreply.github.com>
2024-10-28 15:50:11 +0700

    still error

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const postController = require('../controllers/postController');
 const router = express.Router();
 
-router.post('/api/posts', postController.createPost);       // Tạo bài viết
-router.get('/api/posts', postController.getAllPosts);       // Xem toàn bộ bài viết
-router.get('/api/posts/:postId', postController.getPostById); // Xem bài viết theo ID
-router.put('/api/posts/:postId', postController.updatePost);  // Cập nhật bài viết
-router.delete('/api/posts/:postId', postController.deletePost); // Xóa bài viết
+router.post('/posts', postController.createPost);       // Tạo bài viết
+router.get('/posts', postController.getAllPosts);       // Xem toàn bộ bài viết
+router.get('/posts/:postId', postController.getPostById); // Xem bài viết theo ID
+router.put('/posts/:postId', postController.updatePost);  // Cập nhật bài viết
+router.delete('/posts/:postId', postController.deletePost); // Xóa bài viết
 
 module.exports = router;
