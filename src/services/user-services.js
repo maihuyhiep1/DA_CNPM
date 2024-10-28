0ac95dc4d77da1d8e6a002576a016136bf5247d7
KhoaPham359 <145217965+KhoaPham359@users.noreply.github.com>
2024-10-28 15:33:55 +0700

    Update user-services.js

--- src/services/user-services.js
+++ src/services/user-services.js
@@ -80,7 +80,7 @@ let getUserInfoByID = (id) => {
 /**
  * Updates user information for a given user ID.
  * 
- * Field name should be the same as ones stored in database.
+ * Field name must be the same as ones stored in database.
  * 
  * @param {number} id - The ID of the user to update.
  * @param {Object} [updateData] - An object containing the fields to update, can be leaved as null.
