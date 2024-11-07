11746f9e775027f20982136c34ae23f3dfb9184a
KeHamTruyen <146626863+KeHamTruyen@users.noreply.github.com>
2024-11-08 04:58:02 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
-});
+}).promise();
 
 db.connect((err) => {
   if (err) {
