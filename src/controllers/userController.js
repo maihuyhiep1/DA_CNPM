e69edc612ec1fe767ec6e817baec73b98773fe22
KhoaPham359 <phamminhkhoa255204@gmail.com>
2024-11-14 18:29:39 +0700

    session configuration

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,29 +1,46 @@
 const userService = require('../services/user-services');
+const passport = require('passport');
 //API
 
 
 
 // localhost:{port}/api/login
-let handleLogin = async(req, res) => {
-  const {username, password} = req.body;
+let handleLogin = async(req, res, next) => {
+  const { username, password } = req.body;
   if (!username || !password) {
-    return res.status(500).json({
-      errCode:4,
-      message: 'Missing parameter.',
-    })
+    return res.status(400).json({
+      errCode: 4,
+      message: "Missing parameter.",
+    });
   }
-  let userData = await userService.handleUserLogin(username, password);
-  return res.status(200).json({
-    errCode: userData.errCode,
-    message: userData.message,
-    user: userData.user ? userData.user : {},
-  })
+  passport.authenticate("local", async (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      return res.status(400).json({
+        errCode: info.errCode || null,
+        message: info.message || null,
+      });
+    }
+
+    // If user is authenticated, store user info in session
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      // Send the user data (excluding password) in the response
+      return res.status(200).json({
+        errCode: 0,
+        message: 'Ok',
+        user: user,
+      });
+    });
+  })(req, res, next);
 }
 
 //localhost{port}/api/create-new-user
 let handleUserSignin_sentAuthCode = async (req, res) => {
       if (!req.body.email || !req.body.password || !req.body.username || !req.body.name) {
-        return res.status(500).json({
+        return res.status(400).json({
           errCode:4,
           message: 'Missing parameter.',
         })
@@ -38,7 +55,7 @@ let handleUserSignin_sentAuthCode = async (req, res) => {
 
 let handleUserSignin_verifyAuthCode = async (req, res) => {
       if (!req.body.email || !req.body.password || !req.body.username || !req.body.name || !req.body.code) {
-        return res.status(500).json({
+        return res.status(400).json({
           errCode:4,
           message: 'Missing parameter.',
         })
