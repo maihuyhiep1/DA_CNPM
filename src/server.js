0453d86a52b70f4bf95372b5b32d43f5f65d4bb0
KhoaPham359 <phamminhkhoa255204@gmail.com>
2024-11-15 23:13:54 +0700

    get secret key from .env

--- src/server.js
+++ src/server.js
@@ -11,6 +11,8 @@ const session = require('express-session');
 const cors = require('cors');
 const app = express();
 
+
+
 const corsOptions = {
   origin: 'http://localhost:3001', // Change to the frontend's URL (e.g., http://localhost:3001 for frontend on port 3001)
   methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Methods you want to allow
@@ -19,7 +21,7 @@ const corsOptions = {
 };
 
 app.use(session({
-  secret: 'yourSecretKey', // Replace with a strong secret
+  secret: process.env.SESSION_SECRET_KEY,
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false } // Set to `true` for HTTPS
@@ -33,8 +35,9 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-initWebRoutes(app);
 connectDB();
+
+initWebRoutes(app);
 
 
 const port = process.env.PORT || 3000;
