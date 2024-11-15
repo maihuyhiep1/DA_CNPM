cffc8c34ccf397d589a5cc5647a4035301d8268c
KhoaPham359 <phamminhkhoa255204@gmail.com>
2024-11-15 23:14:22 +0700

    synchronize with database feature

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,26 @@
 
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-
+const db = require('../models/index.js');
 
 
 const {HOST, DATABASE, DB_USERNAME, DB_PASSWORD} = process.env;
 const sequelize = new Sequelize(DATABASE, DB_USERNAME, DB_PASSWORD, {
   host: HOST,
-  dialect: 'mysql' 
+  dialect: 'mysql',
 });
+
 
 let connectDB = async() => {
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log('Connected to database.');
+    await db.sequelize.sync({
+      alter: true,
+      logging: false
+    })
+      .then(()=>{ console.log('Database synchronized.')})
+      .catch(err => console.error('Error:', err));
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
