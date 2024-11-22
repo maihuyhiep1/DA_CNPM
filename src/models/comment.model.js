3c32b89e0a70ac32dfd020ff5c58251127ee7ed1
KhoaPham359 <145217965+KhoaPham359@users.noreply.github.com>
2024-11-22 19:03:41 +0700

    update

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,42 +1,51 @@
 'use strict';
+const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  const Comment = sequelize.define(
-    'Comment',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      content: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      postId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      commentId: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-      },
-    },
-    {
-      timestamps: true, // Automatically adds `createdAt` and `updatedAt` columns
-    }
-  );
 
+class Comment extends Model {
+  static associate(models) {
+    // Define associations here
+  
   // Comment.associate = (models) => {
   //   // Define associations here
   //   Comment.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
   //   Comment.belongsTo(models.Post, { foreignKey: 'postId', as: 'post' });
   //   Comment.hasMany(models.Comment, { foreignKey: 'commentId', as: 'replies' });
   // };
+  }
+}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    commentId: {
+      type: DataTypes.INTEGER,
+      allowNull: true, // For replies to other comments
+    },
+  },
+  {
+    sequelize, // Pass the Sequelize instance
+    modelName: 'Comment',
+    timestamps: true, // Automatically adds `createdAt` and `updatedAt`
+  }
+);
 
   return Comment;
-};
+}
