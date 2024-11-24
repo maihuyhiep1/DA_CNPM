884043e7e496e0da1f0a3865f5e71bb71e039add
KeHamTruyen <146626863+KeHamTruyen@users.noreply.github.com>
2024-11-25 04:05:17 +0700

    update cmt

--- src/route/commentRoutes.js
+++ src/route/commentRoutes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const commentController = require('../controllers/commentController');
-
+const isAuthenticated = require('../middlewares/auth');
 // Comment routes
-router.post("/post/:postId", commentController.create); // Thêm comment vào bài viết
-router.put("/:id", commentController.update); // Cập nhật comment
-router.delete("/:id", commentController.delete); // Xóa comment
+router.post("/post/:postId",isAuthenticated, commentController.create); // Thêm comment vào bài viết
+router.put("/:id",isAuthenticated, commentController.update); // Cập nhật comment
+router.delete("/:id",isAuthenticated, commentController.delete); // Xóa comment
 router.get("/", commentController.findAll); // Lấy tất cả comment
 router.get("/post/:id", commentController.findAllofPostId); // Lấy tất cả comment của bài viết
 
