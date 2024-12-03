3feedcb5a108f8a4f643a8920b663ddbd9684037
KeHamTruyen <146626863+KeHamTruyen@users.noreply.github.com>
2024-12-04 06:24:19 +0700

    thêm thanh tìm kiếm (nằm trong postcontroller

--- src/route/postRoutes.js
+++ src/route/postRoutes.js
@@ -58,10 +58,12 @@ router.post(
 );
 
 // Route xem bài viết
+router.get('/posts/search', postController.searchPosts);
 router.get('/users/:userId/posts', postController.getPostsByUser);
 router.get('/posts', postController.getAllPosts);               // Lấy danh sách bài viết
 router.get('/posts/:postId', postController.getPostById);       // Lấy bài viết theo ID
 router.get('/posts/popular', postController.getPopularPosts);
+
 // Các route yêu cầu xác thực session
 router.post(
     '/posts',
