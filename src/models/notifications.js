b433800fc6028f807f96937fd78fab030ddc3ec0
Lê Khoa <lekhoa@MacBook-Pro-cua-Le.local>
2024-12-06 16:47:10 +0700

    notices

--- src/models/notifications.js
+++ src/models/notifications.js
@@ -2,45 +2,49 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  class Notification extends Model {
-    static associate(models) {
-      this.belongsTo(models.Post, {
-        foreignKey: 'post_id',
-        as: 'post',
-        onDelete: 'CASCADE', 
-      });
+    class Notification extends Model {
+        static associate(models) {
+            this.belongsTo(models.Post, {
+                foreignKey: 'post_id',
+                as: 'post',
+                onDelete: 'CASCADE',
+            });
 
-      this.belongsTo(models.User, {
-        foreignKey: 'user_id',
-        as: 'user',
-        onDelete: 'CASCADE', 
-      });
+            this.belongsTo(models.User, {
+                foreignKey: 'user_id',
+                as: 'user',
+                onDelete: 'CASCADE',
+            });
+        }
     }
-  }
 
-  Notification.init(
-    {
-      user_id: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        primaryKey: true, // Khóa chính kết hợp với post_id
-      },
-      post_id: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        primaryKey: true,
-      },
-      content: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    },
-    {
-      sequelize,
-      modelName: 'Notification', // Tên mô hình
-      tableName: 'notification', // Tên bảng trong cơ sở dữ liệu
-    }
-  );
+    Notification.init(
+        {
+            noti_id: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                defaultValue: DataTypes.UUIDV4,
+                primaryKey: true,
+            },
+            user_id: {
+                type: DataTypes.UUID,
+                allowNull: false,
+            },
+            post_id: {
+                type: DataTypes.UUID,
+                allowNull: false,
+            },
+            content: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            sequelize,
+            modelName: 'Notification', // Tên mô hình
+            tableName: 'notification', // Tên bảng trong cơ sở dữ liệu
+        }
+    );
 
-  return Notification;
+    return Notification;
 };
