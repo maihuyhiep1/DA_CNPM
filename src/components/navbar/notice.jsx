d7e6ac0dca8f0f930c3f78a99e65205662aab874
Lê Khoa <lekhoa@MacBook-Pro-cua-Le.local>
2024-12-06 16:19:21 +0700

    notice

--- src/components/navbar/notice.jsx
+++ src/components/navbar/notice.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext  } from 'react';
+import axios from 'axios';
 import styles from './style_notice.module.css'; // Import the CSS module
+import { AuthContext } from "../../context/authContext";
 
 const Notice = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [notices, setNotices] = useState([]);
+  const { currentUser } = useContext(AuthContext);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -14,7 +18,23 @@ const Notice = () => {
     }
   };
 
-  React.useEffect(() => {
+  // Fetch notices
+  useEffect(() => {
+    const fetchNotices = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8386/api/notices/${currentUser.id}`);
+        if (response.data.success) {
+          setNotices(response.data.data);
+        }
+      } catch (error) {
+        console.error("Error fetching notices:", error);
+      }
+    };
+
+    fetchNotices();
+  }, []);
+
+  useEffect(() => {
     window.addEventListener('click', closeDropdown);
     return () => {
       window.removeEventListener('click', closeDropdown);
@@ -24,21 +44,27 @@ const Notice = () => {
   return (
     <div>
       <div className={styles.dropdown}>
-        <img              
-         src="img_navbar/notice.png"
+        <img
+          src="img_navbar/notice.png"
           onClick={toggleDropdown}
           className={styles.dropbtn}
-        >
-        </img>
+          alt="Notice Icon"
+        />
 
         <div
           className={`${styles['dropdown-content']} ${
             isOpen ? styles.show : ''
           }`}
         >
-          <a href="#home">Trangcácácácácácá cá nhân</a>
-          <a href="#about">Cài đặt</a>
-          <a href="#contact">Đăng xuất</a>
+          {notices.length > 0 ? (
+            notices.map((notice, index) => (
+              <a key={index} href={`/post/${notice.post_id}`}>
+                {notice.content}
+              </a>
+            ))
+          ) : (
+            <p className={styles.noNotices}>Không có thông báo nào</p>
+          )}
         </div>
       </div>
     </div>
