4e7acd21142e2cfd71c54ea62386926929909bfd
KhoaPham359 <145217965+KhoaPham359@users.noreply.github.com>
2024-12-12 21:22:48 +0700

    update googleAuthController to fix redirect URL and correct date formatting for updatedAt

--- src/controllers/googleAuthController.js
+++ src/controllers/googleAuthController.js
@@ -2,14 +2,14 @@ require("passport");
 let callbackUser = (req, res) => {
   // res.redirect(`${process.env.CLIENT_URL}/login-success/${req.user?.id}`);
 
-  res.redirect(process.env.CLIENT_URL)
+  res.redirect(`${process.env.CLIENT_URL}/login-success`)
 }
 
 const loginSuccess = async (req, res) => {
     if (req.isAuthenticated()) {
       const user = req.user;
       user.setDataValue('createdAt', new Date(user.createdAt).toLocaleDateString('en-GB'));
-      user.setDataValue('updatedAt', new date(user.updatedAt).toLocaleDateString('en-GB'));
+      user.setDataValue('updatedAt', new Date(user.updatedAt).toLocaleDateString('en-GB'));
       res.status(200).json({
         errCode: 0,
         message: "Ok",
